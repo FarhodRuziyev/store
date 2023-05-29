cdce0a0e17a3e611d7af11d2773a619741dcb1b8
shohzod3112 <shohzod3112@gmail.com>
2023-05-29 12:16:10 +0500

    portfolio added

--- myapp/src/pages/about/index.jsx
+++ myapp/src/pages/about/index.jsx
@@ -7,7 +7,7 @@ export default function About() {
         <div>
           <div>
             <h1>Lorem ipsum dolor sit amet consectetur</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias culpa tempore natus cumque? Assumenda nesciunt iusto sequi, vero, nam harum cupiditate et consectetur aperiam vel omnis eveniet delectus rerum! Consequuntur repudiandae exercitationem autem amet architecto numquam eum sunt</p>
+            <p>Lorem ipsum, dolor sit amet  adipisicing elit. Alias culpa tempore natus cumque? Assumenda nesciunt iusto sequi, vero, nam harum cupiditate et consectetur aperiam vel omnis eveniet delectus rerum! Consequuntur repudiandae exercitationem autem amet architecto numquam eum sunt</p>
             <a href="http://"> malumot </a>
           </div>
         </div>
